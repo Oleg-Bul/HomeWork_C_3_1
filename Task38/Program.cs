b12205f44579bbc136c136d6e433d30ca1557c46
Oleg_bul <kokseg63@gmail.com>
2022-09-30 18:28:13 +0400

    task 38 complete

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+int ReadData(string msg)
+{
+    System.Console.Write(msg);
+    int number = int.Parse(System.Console.ReadLine() ?? "0");
+    return number;
+}
+
+double[] CreateArr(int arrLen)
+{
+    double[] array = new double[arrLen];
+    Random rand = new Random();
+    for (int i = 0; i < arrLen; i++)
+    {
+        array[i] = rand.Next(-99, 99) + rand.NextDouble();
+    }
+    return array;
+}
+
+double NumberOfEvenNumbers(double[] arr)
+{
+    double result =0;
+    double min = arr[0];
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        
+        if(arr[i] < min) min = arr[i];
+        if(arr[i] > max) max = arr[i];
+    }
+    result = max-min;
+    return result;
+}
+
+void PrintData(string msg, double value)
+{
+    Console.WriteLine(msg + value);
+}
+int arrLen = ReadData("Введите длину массива: ");
+double[] arr = CreateArr(arrLen);
+void Print1DArr(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + "]");
+}
+Print1DArr(arr);
+System.Console.WriteLine();
+PrintData("Разница между максимальным и минимальным значениями массива равна: " , NumberOfEvenNumbers(arr));
